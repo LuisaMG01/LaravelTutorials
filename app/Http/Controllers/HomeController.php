d928ebad31ca4a8dcff4cff802a13822873c4403
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2024-03-05 19:55:07 -0500

    cart functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,32 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+
+    public function about(): View
+    {
+        $data1 = "About us - Online Store";
+        $data2 = "About us";
+        $description = "This is an about page ...";
+        $author = "Developed by: Your Name";
+        return view('home.about')
+            ->with("title", $data1)
+            ->with("subtitle", $data2)
+            ->with("description", $description)
+            ->with("author", $author);
+    }  
+
+    public function contact(): View
+    {
+        $data1 = "email@email.com";
+        $data2 = "address # address - address";
+        $data3 = "123455678";
+        $title = "This is the contact section";
+        $subtitle = "There is the information about us";
+        return view('home.contact') -> with("email", $data1)
+        ->with("address", $data2)
+        ->with("phone_number", $data3)
+        ->with("title", $title)
+        ->with("subtitle", $subtitle);
+    }
+    
 }
